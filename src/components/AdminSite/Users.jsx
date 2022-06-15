70ff6bd92d17500434fa2b9329eeee0f0a0f0e64
Santiago Andrés Testa <testasanty@gmail.com>
2022-06-15 12:12:55 -0300

    Cleanup UserPage

--- src/components/AdminSite/Users.jsx
+++ src/components/AdminSite/Users.jsx
@@ -52,11 +52,6 @@ export default function Users() {
 
                   <td>
                     <div className="flex items-center space-x-3">
-                      {/* <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                        <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-                        </div>
-                      </div> */}
                       <div>
                         <div className="font-bold">{u.nickName}</div>
                         <div className="text-sm opacity-50">{u.country}</div>
@@ -68,12 +63,12 @@ export default function Users() {
                     <br />
                     <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
                   </td>
-                      <th>
-                        <button id={u.email} name={u.usertype} onClick={e => handlePermission(e)}>Change to {u.usertype === 'Admin' ? 'User' : 'Admin'}</button>
-                      </th>
+                  <th>
+                    <button id={u.email} name={u.usertype} onClick={e => handlePermission(e)}>Change to {u.usertype === 'Admin' ? 'User' : 'Admin'}</button>
+                  </th>
                 </tr>
               })
-              : <p>wuachin</p>
+              : null
             }
           </tbody>
           {/* <!-- foot --> */}
