2f9a45e8756c3ab1401a582ede93da6c7eb575d2
sedwarb <sedwarb@gmail.com>
2022-06-15 14:48:25 -0500

    added sedes

--- src/components/Map/Maps.jsx
+++ src/components/Map/Maps.jsx
@@ -14,15 +14,9 @@ async function location(){
 let sucursal,suc,p1=location()
 
 export default function Maps(){
-  const [sucursals, setSucursals] = useState(0);
+  //const [sucursals, setSucursals] = useState(0);
   return (
-    <div className="container">
-      <select key="22" name="listaOrden" id="listaOrden" onChange={(e)=>setSucursals(e.currentTarget.value)} >
-        {sucursal.map((s,i)=>{
-            return <option key={`map30${i}`} value={i} >{s.name}</option>
-        })}
-      </select>
-      <br/>
+    <div className="container">      
       <GoogleMaps
         apiKey={"AIzaSyBDaeWicvigtP9xPv919E-RNoxfvC-Hqik"}
         style={{ height: "400px", width: "100%" }}
@@ -30,8 +24,24 @@ export default function Maps(){
         
         center={{lat: 3.4168994,lng: -76.5260932}}
         markers={suc}
-        
       />
+      <br/>
+      <div>
+        <h3>Nuestras Sedes:</h3>
+        <p>Estadium: Cl. 10 #37 - Cali, Valle del Cauca - Colombia</p>
+        <p>El Dorado: Cl. 19 ##37-25 Local 18 - Cali, Valle del Cauca - Colombia</p>
+        <p>Paso Ancho: Cl. 13c #41-31 - Cali, Valle del Cauca - Colombia</p>
+      </div>
     </div>
   );
 }
+
+/*
+<select key="22" name="listaOrden" id="listaOrden" onChange={(e)=>setSucursals(e.currentTarget.value)} >
+        {sucursal.map((s,i)=>{
+            return <option key={`map30${i}`} value={i} >{s.name}</option>
+        })}
+      </select>
+      <br/>
+
+*/
