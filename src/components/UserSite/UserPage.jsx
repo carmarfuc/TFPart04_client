8e1d40a8ac52a91c5011ef7cc5b5b9f280be6e0b
Santiago Andrés Testa <testasanty@gmail.com>
2022-06-15 14:52:34 -0300

    UserSite template

--- src/components/UserSite/UserPage.jsx
+++ src/components/UserSite/UserPage.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import { getUserOrders } from '../../redux/actions'
 
 const UserPage = () => {
 
+  const user = localStorage.getItem("user")
+  console.log('User: ',user)
+
+  // async function handleGetOrders(user) {
+  //   const orders = await dispatch(getUserOrders(user))
+  //   console.log('Orders: ',orders)
+  // }
 
   return (
-    <div>User page pana</div>
+    <div>
+      <div className="overflow-x-auto w-full z-50">
+        <table className="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>User type</th>
+              <th>Permissions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <tr>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div>
+                    <div className="font-bold">{u.nickName}</div>
+                    <div className="text-sm opacity-50">{u.country}</div>
+                  </div>
+                </div>
+              </td>
+              <td>
+                {u.usertype}
+                <br />
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+              </td>
+              <th>
+                <button id={u.email} name={u.usertype} onClick={e => handlePermission(e)}>Change to {u.usertype === 'Admin' ? 'User' : 'Admin'}</button>
+              </th>
+            </tr> */}
+          </tbody>
+          {/* <!-- foot --> */}
+        </table>
+      </div>
+      <div>
+      </div>
+    </div>
   )
 }
 
